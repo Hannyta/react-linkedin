6b2278e5b4325c3e67435212082d352304402b42
Hanny <Hannylinares@gmail.com>
2025-09-03 10:01:22 -0300

    Comunicación entre componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import './App.css'
 import ComponenteA from './components/ComponenteA';
 import ComponenteB from './components/ComponenteB';
+import { useState, useCallback } from 'react';
 
 function App() {
 
   const miEmoji = "✈"
   const miBoton = "Buscar destino"
 
+  const manejarClick = () => {
+    setMensaje("Listo para viajar")
+  }
+
+  const [mensaje, setMensaje] = useState("Viajes Aventura");
+
+  const callback = useCallback(
+
+    (datos)=>{
+      setMensaje(datos)
+    }, [mensaje]
+  )
+
   return (
     <>
       <div className='box'>
         <ComponenteA
           emoji={miEmoji}
-          titulo="Viajes Aventura"
+          titulo={mensaje}
         />
         <ComponenteB
           textoBtn= {miBoton}
+          onClick={manejarClick}
+        />
+        <ComponenteB
+          textoBtn= "Callback"
+          onClick={callback}
         />
       </div>
     </>
